dffda68021ff31538df313bf02365b4b28f03a65
mykola-mmm <mykola.mazniuk@gmail.com>
2024-10-19 03:04:37 +0200

    testing

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,7 +3,7 @@ import pandas as pd
 import tensorflow as tf
 
 
-def classification_data_loader(df_x, df_y, dataset_path, batch_size=32, validation=False):
+def classification_data_loader(df_x, df_y, dataset_path, batch_size=32):
     df = pd.concat([df_x, df_y], axis=1)
     df = df.sample(frac=1,random_state=42).reset_index(drop=True)
     # print(df)
@@ -19,12 +19,29 @@ def classification_data_loader(df_x, df_y, dataset_path, batch_size=32, validati
                 image = tf.cast(image, tf.float32) / 255.0
                 images.append(image)
                 labels.append(row['HasShip'])
-            if not validation:
-                yield tf.stack(images), tf.stack(labels)
-            else:
-                yield (tf.stack(images), tf.stack(labels))
+
+            yield tf.stack(images), tf.stack(labels)
 
 # def classification_validation_data_loader(df_x, df_y, dataset_path, batch_size=32):
 #     while True:
 #         x, y = next(classification_data_loader(df_x, df_y, dataset_path, batch_size))
 #         yield (x, y)
+
+def classification_validation_data_loader(df_x, df_y, dataset_path, batch_size=32):
+    df = pd.concat([df_x, df_y], axis=1)
+    df = df.sample(frac=1,random_state=42).reset_index(drop=True)
+    # print(df)
+    while True:
+        for i in range(0, len(df), batch_size):
+            batch_df = df.iloc[i:i+batch_size]
+            images = []
+            labels = []
+            for _, row in batch_df.iterrows():
+                image_path = os.path.join(dataset_path, f"{row['ImageId']}")
+                image = tf.io.read_file(image_path)
+                image = tf.image.decode_jpeg(image, channels=3)
+                image = tf.cast(image, tf.float32) / 255.0
+                images.append(image)
+                labels.append(row['HasShip'])
+
+            yield tf.stack(images), tf.stack(labels)
