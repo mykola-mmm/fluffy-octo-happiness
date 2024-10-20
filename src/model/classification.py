0b8c9b35cd2e06fef2cdefa297c7ddadaffbd676
mykola-mmm <mykola.mazniuk@gmail.com>
2024-10-20 02:27:21 +0200

    preparing for training

--- src/model/classification.py
+++ src/model/classification.py
@@ -103,8 +103,8 @@ class BinaryClassificationCNN(tf.keras.Model):
             steps_per_epoch=train_steps_per_epoch,
             validation_data=validation_data,
             validation_steps=val_steps_per_epoch,
-            # callbacks=[reduce_lr_callback, tensorboard_callback],
-            callbacks=[checkpoint_callback, reduce_lr_callback, tensorboard_callback],
+            callbacks=[reduce_lr_callback, tensorboard_callback],
+            # callbacks=[checkpoint_callback, reduce_lr_callback, tensorboard_callback],
             class_weight={0: weight_zero, 1: weight_one}
         )
 
