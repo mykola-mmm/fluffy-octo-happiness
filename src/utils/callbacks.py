9f96164b6cb543245da1c3f44c9822eed24e6bb2
mykola-mmm <mykola.mazniuk@gmail.com>
2024-10-25 04:10:26 +0200

    fixing model save callback

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -10,17 +10,18 @@ class CustomModelCheckpoint(tf.keras.callbacks.ModelCheckpoint):
     def _save_model(self, epoch, batch, logs):
         # Delete previous model files if they exist
         if self.best_filepath:
-            model_dir = os.path.dirname(self.best_filepath)
-            # Get the base filename without epoch and metrics
-            base_filename = os.path.basename(self.best_filepath).split('model_')[0]
+            os.remove(self.best_filepath)
+            # model_dir = os.path.dirname(self.best_filepath)
+            # # Get the base filename without epoch and metrics
+            # base_filename = os.path.basename(self.best_filepath).split('model_')[0]
             
-            # Find and delete previous model files
-            model_pattern = os.path.join(model_dir, f"model_*.keras")
-            for old_model in glob.glob(model_pattern):
-                try:
-                    os.remove(old_model)
-                except OSError as e:
-                    print(f"Error deleting file {old_model}: {e}")
+            # # Find and delete previous model files
+            # model_pattern = os.path.join(model_dir, f"model_*.keras")
+            # for old_model in glob.glob(model_pattern):
+            #     try:
+            #         os.remove(old_model)
+            #     except OSError as e:
+            #         print(f"Error deleting file {old_model}: {e}")
         
         # Save new best model
         filepath = self._get_file_path(epoch, batch, logs)
